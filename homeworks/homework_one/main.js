009b9b954e866a9259a8c3189c5b437f10d73e1f
Orestarxx <oresto322@gmail.com>
2024-12-03 01:08:14 +0200

    done

--- homeworks/homework_one/main.js
+++ homeworks/homework_one/main.js
@@ -1,1 +1,13 @@
-console.log('hello world')
+
+const https = require('node:https');
+
+https.get('https://jsonplaceholder.typicode.com/posts', (res) => {
+    console.log(res)
+
+    res.on('data', (d) => {
+        process.stdout.write(d);
+    });
+
+}).on('error', (e) => {
+    console.error(e);
+});
